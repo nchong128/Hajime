f96b6d3b23adb03333a91d34344c702326c5d693
Nicholas Lin Ren Chong <ncho0010@student.monash.edu>
2020-09-14 01:37:44 +1200

    add tailwindclient

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      Hello World
+    </h1>
+  );
+}
+
+export default App;
